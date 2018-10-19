81c1b7a7d496685984a0a51070357b9a481e7d71
kharrak <vcosmin.lupu@yahoo.com>
2018-10-19 13:05:37 +0300

    upgrade lib to net standar 2.0 and all other packages (#1)
    fixed param namings in method summary

--- UrlCombineLib/UriExtension.cs
+++ UrlCombineLib/UriExtension.cs
@@ -7,7 +7,7 @@ namespace UrlCombineLib
         /// <summary>
         /// Combines the Uri with a base path and the relative url into one, consolidating the '/' between them
         /// </summary>
-        /// <param name="urlBase">Base Uri that will be combined</param>
+        /// <param name="baseUri">Base Uri that will be combined</param>
         /// <param name="relativeUrl">The relative path to combine</param>
         /// <returns>The merged Uri</returns>
         public static Uri Combine(this Uri baseUri, string relativeUrl)
@@ -21,8 +21,8 @@ namespace UrlCombineLib
         /// <summary>
         /// Combines the Uri with base path and the array of relative urls into one, consolidating the '/' between them
         /// </summary>
-        /// <param name="urlBase">Base Uri that will be combined</param>
-        /// <param name="relativeUrl">The array of relative paths to combine</param>
+        /// <param name="baseUri">Base Uri that will be combined</param>
+        /// <param name="relativePaths">The array of relative paths to combine</param>
         /// <returns>The merged Uri</returns>
         public static Uri Combine(this Uri baseUri, params string[] relativePaths)
         {
